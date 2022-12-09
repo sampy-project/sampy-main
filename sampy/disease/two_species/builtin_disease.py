2a08d9aa5ae64c5f55fcfac58bd1ec6f4ad5c13f
mrgoodfish <fviard440@gmail.com>
2022-12-09 11:52:06 -0500

    two species disease update

--- sampy/disease/two_species/builtin_disease.py
+++ sampy/disease/two_species/builtin_disease.py
@@ -17,12 +17,72 @@ class TwoSpeciesContactCustomProbTransitionPermanentImmunity(BaseTwoSpeciesDisea
                building blocks involved in this disease requires many actions to be performed in a precise order,
                otherwise the model's behaviour cannot be guaranteed. See each simplified method description to learn
                about each respective ordering.
+
+    :param disease_name: mandatory kwargs. String.
+    :param host1: mandatory kwargs. Population object of the first host.
+    :param host2: mandatory kwargs. Population object of the second host.
     """
     def __init__(self, **kwargs):
         pass
 
-    def simplified_contact_contagion(self):
-        pass
+    def simplified_contact_contagion(self, contact_rate_matrix, arr_timesteps_host1, arr_prob_timesteps_host1,
+                                     arr_timesteps_host2, arr_prob_timesteps_host2,
+                                     position_attribute_host1='position', position_attribute_host2='position',
+                                     condition_host1=None, condition_host2=None,
+                                     return_arr_new_infected=False, return_type_transmission=False):
+        """
+        Propagate the disease by direct contact using the following methodology. For any vertex X of the graph, we
+        count the number of contagious agents N_c_host_j, then each non immuned agent of host_k on the vertex X has a
+        probability of
+
+                            1 - (1 - contact_rate_matrix[j,k]) ** N_c_host_j
+
+        to become infected by a contact with an agent of host_j.
+
+        Detailed Explanation: each agent has a series of counter attached, telling how much time-steps they will spend
+                              in each disease status. Those counters have to be initialized when an individual is newly
+                              infected, and that's what this method does to the newly infected individuals.
+
+        :param contact_rate_matrix: 2D array of floats of shape (2, 2). Here, contact_rate_matrix[0][0] is the
+                                    probability of contact contagion from host1 to host1, contact_rate_matrix[0][1] is
+                                    the probability of contact contagion from host1 to host2, etc...
+        :param arr_timesteps_host1: 1d array of int. work in tandem with arr_prob_timesteps_host1, see below.
+        :param arr_prob_timesteps_host1: 1D array of float. arr_prob[i] is the probability for an agent to stay infected
+                                         but not contagious for arr_nb_timestep[i] time-steps.
+        :param arr_timesteps_host2: same but for host2
+        :param arr_prob_timesteps_host2: same but for host2
+        :param position_attribute_host1: optional, string, default 'position'. Name of the agent attribute used as
+                                         position for host1.
+        :param position_attribute_host2: optional, string, default 'position'. Name of the agent attribute used as
+                                         position for host2.
+        :param condition_host1: optional, array of bool, default None. Array of boolean such that the i-th value is
+                          True if and only if the i-th agent of host1 (i.e. the agent at the line i of df_population)
+                          can be infected and transmit disease. All the agent having their corresponding value to False
+                          are protected from infection and cannot transmit the disease.
+        :param condition_host2: optional, array of bool, default None. Array of boolean such that the i-th value is
+                          True if and only if the i-th agent of host2 (i.e. the agent at the line i of df_population)
+                          can be infected and transmit disease. All the agent having their corresponding value to False
+                          are protected from infection and cannot transmit the disease.
+        :param return_arr_new_infected: optional, boolean, default False. If True, the method returns two arrays telling
+                                        which agent got contaminated in each host species.
+        :param return_type_transmission: optional, boolean, default False.
+
+        :return: Depending on the values of the parameters 'return_arr_new_infected' and 'return_type_transmission',
+                 the return value will either be None (both are False) or a dictionnary whose key-values are:
+                    - 'arr_new_infected_host1', 'arr_new_infected_host2' if return_arr_new_infected is True, and the
+                      values are 1D arrays of bool telling which agents got infected for each host;
+                    - 'arr_type_transmission_host1', 'arr_type_transmission_host2' if return_type_transmission is True,
+                      and the values are 1D arrays of non-negative integers such that the integer at line i is 0 if the
+                      i-th agent has not been contaminated, 1 if it has been contaminated by a member of its own species
+                      2 if it has been contaminated by a member of the other species, 3 if it has been contaminated by
+                      agents from both its species and the other.
+        """
+        dict_contagion = self.contact_contagion(contact_rate_matrix, return_arr_new_infected=True,
+                                                return_type_transmission=return_type_transmission,
+                                                position_attribute_host1=position_attribute_host1,
+                                                position_attribute_host2=position_attribute_host2,
+                                                condition_host1=condition_host1, condition_host2=condition_host2)
+
 
     def simplified_transition_between_states(self):
         pass

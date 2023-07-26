2ec136e57ff2007f741d8d2ba4764af6f8870ba2
sampy-project <sampy.project@gmail.com>
2023-07-26 13:45:48 -0400

    work on Base-intersection

--- sampy/spatial/base_graph_intersection.py
+++ sampy/spatial/base_graph_intersection.py
@@ -3,11 +3,26 @@ import numpy as np
 
 class BaseGraphIntersection:
     """
-    todo
+    This class defines the data structures encoding the intersection of a pair of graph (g1, g2). 
+
+    Formally, we consider graphs such that each vertex v come with some area/volume a_v attached to it.
+    Then the intersection of g1 to g2 is way to know, for each vertex v in g_j, which are the vertices
+    [w_1, w_2, ..., w_n] in g_i such that the a_v and a_w_k intersect for all k, and to know the proportion
+    of a_v in this intersection.
+
+    Given that the number 
 
     Define the basic attributes.
         graph_1: graph object
         graph_2: graph object
+
+        indexes_g1_to_g2: 1D array of integers
+        connections_g1_to_g2: 1D array of integers
+        weights_g1_to_g2: 1D array of floats
+
+        indexes_g2_to_g1: 1D array of integers
+        connections_g2_to_g1: 1D array of integers
+        weights_g2_to_g1: 1D array of floats
 
     """
     def __init__(self, graph_1=None, graph_2=None, **kwargs):
@@ -25,8 +40,6 @@ class BaseGraphIntersection:
         self.indexes_g2_to_g1 = None
         self.connections_g2_to_g1 = None
         self.weights_g2_to_g1 = None
-
-        pass
 
 
 
